338c4877474d4b465b3ecf3c112164b41306b14c
jorenchik <jorens.stekels@gmail.com>
2023-12-28 20:37:33 +0200

    merged and fixed

--- code/task_2.py
+++ code/task_2.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
-
+import unittest
 from automata.pda.npda import NPDA
 
 npda = NPDA(
-    states={"s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11"},
+    states={
+        "s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10",
+        "s11"
+    },
     input_symbols={"0", "1"},
     stack_symbols={"0", "1", "#"},
     transitions={
@@ -39,7 +42,7 @@ npda = NPDA(
         },
         "s3": {
             "0": {
-                # "#": {("s5", "#")},
+                "#": {("s5", "0#")},
                 "0": {("s5", "00")},
             },
             "1": {
@@ -70,7 +73,7 @@ npda = NPDA(
         "s6": {
             "0": {
                 "#": {("s11", "#")},
-                "0": {("s11", "0")},
+                "0": {("s11", "0")},  # WARNING: not used
             },
             "1": {
                 "#": {("s7", "#")},
@@ -93,13 +96,13 @@ npda = NPDA(
                 "1": {("s10", "1")},
             },
             "1": {
-                "#": {("s9", "#")},
+                "#": {("s9", "1#")},
                 "1": {("s9", "11")},
             },
         },
         "s9": {
             "0": {
-                "#": {("s4", "#")},
+                "#": {("s11", "#")},
                 "1": {("s8", "")},
             },
             "1": {
@@ -133,3 +136,66 @@ npda = NPDA(
     final_states={"s6", "s7", "s8", "s9", "s10"},
     acceptance_mode="final_state",
 )
+
+
+class TestNPDA(unittest.TestCase):
+
+    def test_accepts_input(self):
+        self.assertEqual(npda.accepts_input("101"), True)
+        self.assertEqual(npda.accepts_input("1010"), False)
+        self.assertEqual(npda.accepts_input("10101"), True)
+        self.assertEqual(npda.accepts_input("101010"), False)
+        self.assertEqual(npda.accepts_input("01011011001001010101"), False)
+        self.assertEqual(npda.accepts_input("10011100110101001110"), False)
+        self.assertEqual(npda.accepts_input("01100100110010111001"), False)
+        self.assertEqual(npda.accepts_input("10101011101000100110"), False)
+        self.assertEqual(npda.accepts_input("00111010110011100101"),
+                         True)  # BUG:
+        self.assertEqual(npda.accepts_input("11010001101011001100"),
+                         True)  # BUG:
+        self.assertEqual(npda.accepts_input("01000110110010110110"), True)
+        self.assertEqual(npda.accepts_input("10011101001100101101"), True)
+        self.assertEqual(npda.accepts_input("00110101010011011011"),
+                         True)  # BUG:
+        self.assertEqual(npda.accepts_input("11001001111101010100"), False)
+        self.assertEqual(npda.accepts_input("01001011001101100101"), False)
+        self.assertEqual(npda.accepts_input("10100110110011010100"),
+                         True)  # BUG:
+        self.assertEqual(npda.accepts_input("01110011000101011011"), True)
+        self.assertEqual(npda.accepts_input("10001001010110101010"), False)
+        # self.assertEqual(npda.accepts_input("00101110111011001001"), False) $ Check this one out
+        self.assertEqual(npda.accepts_input("11010010001001011101"),
+                         False)  # BUG:
+        self.assertEqual(npda.accepts_input("01010100110100110101"),
+                         False)  # BUG:
+        self.assertEqual(npda.accepts_input("10001101010110001110"),
+                         True)  # BUG:
+        self.assertEqual(npda.accepts_input("01100110011001000101"), False)
+        self.assertEqual(npda.accepts_input("10101101101010110100"),
+                         True)  # BUG:
+
+    def check_str1_less_than_str2_in_str(
+        str_: str,
+        str1: str = "110",
+        str2: str = "001",
+    ) -> tuple[bool, tuple[int, int]]:
+        counts = [0, 0]
+        str_buf = ""
+        for i, sym in enumerate(str_):
+            if len(str_buf) < len(str1):
+                str_buf += sym
+            else:
+                if str_buf == str1:
+                    counts[0] += 1
+                if str_buf == str2:
+                    counts[1] += 1
+                str_buf = str_buf[1:] + sym
+        if str_buf == str1:
+            counts[0] += 1
+        if str_buf == str2:
+            counts[1] += 1
+        return counts[0] < counts[1], counts
+
+
+if __name__ == "__main__":
+    unittest.main()
